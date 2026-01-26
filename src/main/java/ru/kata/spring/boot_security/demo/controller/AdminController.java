abc13953b2ed3efe290bf4b042f39055285b77d3
DrinkWaterAndBeHappy <s4mur41.95@mail.ru>
2026-01-26 15:36:08 +0300

    Rest2

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -12,46 +12,8 @@ import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
 @RequestMapping("/admin")
 public class AdminController {
 
-    private final RoleServiceImpl roleService;
-    private final UserServiceImpl userService;
-
-    @Autowired
-    public AdminController(UserServiceImpl userService, RoleServiceImpl roleService) {
-        this.userService = userService;
-        this.roleService = roleService;
-    }
-
     @GetMapping
     public String adminPanel(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        model.addAttribute("allRoles", roleService.findAll());
-        model.addAttribute("user", new User());
         return "admin";
     }
-
-    @PostMapping("/add")
-    public String addUser(@ModelAttribute User user) {
-        userService.addUser(user);
-        return "redirect:/admin";
-    }
-
-    @PostMapping("/edit")
-    public String editUser(@RequestParam Long id,
-                           @ModelAttribute User user) {
-        userService.changeUser(id,
-                user.getFirstName(),
-                user.getLastName(),
-                user.getAge(),
-                user.getEmail(),
-                user.getPassword(),
-                user.getRoles());
-        return "redirect:/admin";
-    }
-
-    @PostMapping("/delete")
-    public String deleteUser(@RequestParam Long id) {
-        userService.deleteUser(id);
-        return "redirect:/admin";
-    }
 }
-
